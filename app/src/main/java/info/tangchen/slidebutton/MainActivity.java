0b723f08b8518251e76be86ad3abd0e7beb7f835
TangChen <1210208878@qq.com>
2017-11-18 19:55:44 +0800

    功能升级 1.可以在XML文件内设置相应属性（背景色、描述文字） 2.随着滑动，描述文字逐渐透明

--- app/src/main/java/info/tangchen/slidebutton/MainActivity.java
+++ app/src/main/java/info/tangchen/slidebutton/MainActivity.java
@@ -2,6 +2,7 @@ package info.tangchen.slidebutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     SlideButton button;
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.sd);
+
+        button.bindListener(new Slide2TheEndListener() {
+            @Override
+            public void changeState() {
+                Toast.makeText(MainActivity.this, "滑动到底部！", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/info/tangchen/slidebutton/SlideButton.java
+++ app/src/main/java/info/tangchen/slidebutton/SlideButton.java
@@ -2,6 +2,7 @@ package info.tangchen.slidebutton;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -15,76 +16,75 @@ import android.view.View;
 
 public class SlideButton extends View implements View.OnTouchListener {
     private Slide2TheEndListener listener;
-    private float startX, startY;
+    private float startX;
+    private int backgroundColor, circleColor;
+    private String textContent;
 
     public SlideButton(Context context) {
         super(context);
-        initButton(context);
     }
 
     public SlideButton(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        initButton(context);
+        initButton(context, attrs);
     }
 
     public SlideButton(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initButton(context);
+        initButton(context, attrs);
     }
 
     public SlideButton(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        initButton(context);
+        initButton(context, attrs);
     }
 
     public void bindListener(Slide2TheEndListener listener) {
         this.listener = listener;
     }
 
-    private void initButton(Context context) {
+    private void initButton(Context context, AttributeSet attrs) {
         setOnTouchListener(this);
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.SlideButton);
+        backgroundColor = typedArray.getColor(R.styleable.SlideButton_bg, Color.GRAY);
+        circleColor = typedArray.getColor(R.styleable.SlideButton_circleColor, Color.WHITE);
+        textContent = typedArray.getString(R.styleable.SlideButton_text);
+        if(textContent == null)
+            textContent = "";
+        typedArray.recycle();
     }
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
-        float offsetX, offsetY;
+        float offsetX;
         switch (motionEvent.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 startX = motionEvent.getX();
-                startY = motionEvent.getY();
                 break;
             case MotionEvent.ACTION_MOVE:
                 offsetX = motionEvent.getX() - startX;
-                offsetY = motionEvent.getY() - startY;
-
                 if(offsetX > 0) {
                     offset = offsetX;
-                    Message message = new Message();
-                    message.obj = "refresh";
-                    myHandler.sendMessage(message);
+                    refreshUI();
                 }
-
-                //new updateUI().run();
                 break;
             case MotionEvent.ACTION_UP:
+                if(offset + 25 >= maxOffsetX)
+                    listener.changeState();
+
                 offset = 0;
 
-                Message message0 = new Message();
-                message0.obj = "refresh";
-                myHandler.sendMessage(message0);
+                refreshUI();
                 break;
         }
         return true;
     }
 
-    class updateUI implements Runnable{
-
-        @Override
-        public void run() {
-            Message message = new Message();
-            message.obj = "refresh";
-            myHandler.sendMessage(message);
-        }
+    private void refreshUI() {
+        Message message = new Message();
+        message.obj = "refresh";
+        myHandler.sendMessage(message);
     }
 
     @SuppressLint("HandlerLeak")
@@ -100,19 +100,64 @@ public class SlideButton extends View implements View.OnTouchListener {
         }
     };
 
-    float offset = 0.0f;
+    float offset = 0.0f, maxOffsetX;
+    float width, height, offsetX, offsetY;
+    Canvas canvas;
     @SuppressLint("DrawAllocation")
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        this.canvas = canvas;
+        width = getWidth();
+        height = getHeight();
+        offsetY = height / 8;
+        offsetX = width / 21;
+        paint();
+    }
+
+    private void paint() {
+        float r = (getHeight() - offsetY * 2) / 2;
+        maxOffsetX = getWidth() - 2 * (offsetX + r);
+
+        printBackground();
+        printTextContent();
+        printCircle(r);
+    }
+
+    float bottom, top, centerX;
+    private void printTextContent() {
         Paint paint = new Paint();
+        Paint.FontMetricsInt fontMetrics = paint.getFontMetricsInt();
         paint.setColor(Color.RED);
-        RectF background = new RectF(50, 20, getWidth() - 50, getHeight() - 20);
+        int baseline = (int)(bottom + top - fontMetrics.bottom - fontMetrics.top) / 2;
+        // 下面这行是实现水平居中，drawText对应改为传入targetRect.centerX()
+        paint.setTextAlign(Paint.Align.CENTER);
+        paint.setStrokeWidth(3);
+        paint.setTextSize(55);
+        paint.setAlpha(getTextAlpha());
+        canvas.drawText(textContent, centerX, baseline + 20, paint);
+    }
+
+    private void printBackground() {
+        Paint paint = new Paint();
+        paint.setColor(backgroundColor);
+        RectF background = new RectF(offsetX, offsetY, getWidth() - offsetX, getHeight() - offsetY);
         canvas.drawRoundRect(background, 0xfff, 0xfff, paint);
-        paint.setColor(Color.WHITE);
-        float r = (getHeight() - 40) / 2;
-        if(offset >= getWidth() - 200)
-            offset = getWidth() - 200;
-        canvas.drawCircle(r + 50 + offset, r + 20, r, paint);
+        bottom = background.bottom;
+        top = background.top;
+        centerX = background.centerX();
+    }
+
+    private int getTextAlpha() {
+        int base = (int) (255 - (255 * (offset >= maxOffsetX ? maxOffsetX : offset) / maxOffsetX));
+        return base <= 235 ? base + 20 : base;
+    }
+
+    private void printCircle(float r) {
+        Paint paint = new Paint();
+        paint.setColor(circleColor);
+        if(offset >= maxOffsetX)
+            offset = maxOffsetX;
+        canvas.drawCircle(r + offsetX + offset, r + offsetY, r, paint);
     }
 }
