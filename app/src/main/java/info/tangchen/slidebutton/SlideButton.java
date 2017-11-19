5c4ce5e29adb81c0a380a066fdb7c7690220722e
TangChen <1210208878@qq.com>
2017-11-19 11:50:17 +0800

    功能修改 当触摸点x坐标不在圆内时，无法拖动圆

--- app/src/main/java/info/tangchen/slidebutton/SlideButton.java
+++ app/src/main/java/info/tangchen/slidebutton/SlideButton.java
@@ -61,21 +61,27 @@ public class SlideButton extends View implements View.OnTouchListener {
         switch (motionEvent.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 startX = motionEvent.getX();
+
+                if (startX > this.spaceX + 2 * r)
+                    return false;
+
                 break;
             case MotionEvent.ACTION_MOVE: //手指按下，滑动的过程
                 offsetX = motionEvent.getX() - startX;
+
                 if(offsetX > 0) {
                     offset = offsetX;
                     refreshUI();
                 }
+
                 break;
             case MotionEvent.ACTION_UP:
                 if(offset + 25 >= maxOffsetX)
                     listener.changeState();
 
-                offset = 0;
+                for(; offset > 0; offset--)
+                    refreshUI();
 
-                refreshUI();
                 break;
         }
         return true;
@@ -100,25 +106,28 @@ public class SlideButton extends View implements View.OnTouchListener {
         }
     };
 
-    float offset = 0.0f, maxOffsetX;
-    float width, height, offsetX, offsetY;
+    float offset = 0.0f, maxOffsetX, r;
+    float width, height, spaceX, spaceY;
     Canvas canvas;
     @SuppressLint("DrawAllocation")
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         this.canvas = canvas;
-        width = getWidth();
-        height = getHeight();
-        offsetY = height / 8; //y轴留空距离
-        offsetX = width / 21; //x轴留空距离，我这里把宽度分割成了21份，在下面取2份为左右两边的留空距离
+
+        if(width == 0) {
+            width = getWidth();
+            height = getHeight();
+            spaceY = height / 8; //y轴留空距离
+            spaceX = width / 21; //x轴留空距离，我这里把宽度分割成了21份，在下面取2份为左右两边的留空距离
+            r = (getHeight() - spaceY * 2) / 2; //圆形按钮的半径，即为整个view的高度减去上下两部分留空区域的一半
+            maxOffsetX = getWidth() - 2 * (spaceX + r); //圆形按钮圆心最远移动距离，即为整个控件的宽度减去 2 * 左右两侧留空 ＋ 2 * 半径
+        }
+
         paint();
     }
 
     private void paint() {
-        float r = (getHeight() - offsetY * 2) / 2; //圆形按钮的半径，即为整个view的高度减去上下两部分留空区域的一半
-        maxOffsetX = getWidth() - 2 * (offsetX + r); //圆形按钮圆心最远移动距离，即为整个控件的宽度减去 2 * 左右两侧留空 ＋ 2 * 半径
-
         printBackground();
         printTextContent();
         printCircle(r);
@@ -127,6 +136,7 @@ public class SlideButton extends View implements View.OnTouchListener {
     float bottom, top, centerX;
     private void printTextContent() {
         Paint paint = new Paint();
+        paint.setAntiAlias(true);
         Paint.FontMetricsInt fontMetrics = paint.getFontMetricsInt();
         paint.setColor(Color.RED);
         int baseline = (int)(bottom + top - fontMetrics.bottom - fontMetrics.top) / 2;
@@ -140,8 +150,9 @@ public class SlideButton extends View implements View.OnTouchListener {
 
     private void printBackground() {
         Paint paint = new Paint();
+        paint.setAntiAlias(true);
         paint.setColor(backgroundColor);
-        RectF background = new RectF(offsetX, offsetY, getWidth() - offsetX, getHeight() - offsetY);
+        RectF background = new RectF(spaceX, spaceY, getWidth() - spaceX, getHeight() - spaceY);
         canvas.drawRoundRect(background, 0xfff, 0xfff, paint);
         bottom = background.bottom;
         top = background.top;
@@ -157,6 +168,7 @@ public class SlideButton extends View implements View.OnTouchListener {
     private void printCircle(float r) {
         Paint paint = new Paint();
         paint.setColor(circleColor);
+        paint.setAntiAlias(true);
 
         //给偏移量设置一个阈值，即不能超过圆形的最大移动距离，否则会出现圆形控件移出整个滑动部件空间的情况
         if(offset >= maxOffsetX)
@@ -164,6 +176,6 @@ public class SlideButton extends View implements View.OnTouchListener {
 
         //cx：圆心x坐标，为r + x轴留空距离 + 移动的距离，cy：圆心y点坐标，为r＋y轴留空距离
         //整个圆形的滑动过程即为圆形在x轴上的运动，因此只改变圆心的x坐标即可
-        canvas.drawCircle(r + offsetX + offset, r + offsetY, r, paint);
+        canvas.drawCircle(r + spaceX + offset, r + spaceY, r, paint);
     }
 }
